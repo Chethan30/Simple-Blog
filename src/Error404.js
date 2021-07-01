d0fe894923f1706f07d953cdd54265ef1a4f0075
Chethan30 <chetan611611611@gmail.com>
2021-07-01 20:11:47 +0530

    Modified Error page

--- src/Error404.js
+++ src/Error404.js
@@ -3,7 +3,8 @@ const Error404 = () => {
     return ( 
         <div className="Error404">
             <h2>Sorry, Not Sorry!</h2>
-            <p> Want to add this page ? Contact the Developer </p>
+            <p> Sorry for the inconvineince, This project currently runs on a local databse! </p>
+            <p> Updates Coming soon!</p>
             <h5><Link to="/"> Click here </Link> to go back to the home page. </h5>
         </div>
      );
